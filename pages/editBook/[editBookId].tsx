020e14fea2bbef6af1a756aca5b29669108ee4dc
arshad007hossain <arshad417hossain@gmail.com>
2022-10-17 07:45:39 +0600

    first commit

--- pages/editBook/[editBookId].tsx
+++ pages/editBook/[editBookId].tsx
@@ -1,3 +1,79 @@
+// import React, { useState} from "react";
+// import { useRouter } from "next/router";
+// import { useDispatch} from "react-redux";
+// import { useAppDispatch, useAppSelector } from "../../hooks";
+// import { getAllBooks, updateBook} from "../../slices/bookSlice";
+// import { useForm } from "react-hook-form";
+// import { ToastContainer } from "react-toastify";
+
+// const EditBook = () => {
+
+//   const AllBooks = useAppSelector(getAllBooks);
+  
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+
+//   const dispatch = useDispatch();
+//   const router = useRouter();
+//   const editBookId = router.query.editBookId
+
+//   const singlebook = AllBooks['books'].map((x)=> x.id = editBookId)
+  
+//   console.log(singlebook)
+
+//   const onSubmit = (e) => {
+//     // dispatch(updateBook());
+//     router.push("/showBooks");
+//   };
+//   return (
+    
+//     <div>
+//       <ToastContainer/>
+//       <h1 className="text-center">Edit Book</h1>
+//       <div className="d-flex justify-content-center">
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <div className="mb-3">
+//             <label htmlFor="text" className="form-label">
+//               Title
+//             </label>
+//             <input
+//               {...register("title", { required: "Title is required" })}
+//               type="text"
+//               className="form-control"
+//             />
+//             {errors.title?.type === "required" && "Title is required" && (
+//         <p style={{ color: "red" }}>Title is Required.</p>
+//     )}
+//           </div>
+//           <div className="mb-3">
+//             <label htmlFor="text" className="form-label">
+//               Author
+//             </label>
+//             <input
+//               {...register("author", { required: "Author is required" })}
+//               type="text"
+//               className="form-control"
+//             />
+//             {errors.author?.type === "required" && "Author  is required" && (
+//         <p style={{ color: "red" }}>Author is Required.</p>
+//     )}
+//           </div>
+
+//           <button type="submit" className="btn btn-primary">
+//             Update Book
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EditBook;
+
+
 import React, { useState} from "react";
 import { useRouter } from "next/router";
 import { useDispatch} from "react-redux";
@@ -18,14 +94,25 @@ const EditBook = () => {
 
   const dispatch = useDispatch();
   const router = useRouter();
-  const editBookId = router.query.editBookId
+  let editBookId = router.query.editBookId
+  console.log(editBookId);
+  //return;
+  let singlebook='';
+   AllBooks['books'].map((x)=>{
+    if(editBookId==x.id){
+      singlebook=x;
+    }
+  } 
+  )
 
-  const singlebook = AllBooks.map((x)=> x.id = editBookId)
+  //const singlebook = AllBooks['books'];
   
   console.log(singlebook)
 
   const onSubmit = (e) => {
-    // dispatch(updateBook());
+    let data={'id':singlebook['id'],...e};
+    console.log(data)
+    dispatch(updateBook(data));
     router.push("/showBooks");
   };
   return (
@@ -42,6 +129,7 @@ const EditBook = () => {
             <input
               {...register("title", { required: "Title is required" })}
               type="text"
+              // value={singlebook['title']}
               className="form-control"
             />
             {errors.title?.type === "required" && "Title is required" && (
@@ -55,6 +143,7 @@ const EditBook = () => {
             <input
               {...register("author", { required: "Author is required" })}
               type="text"
+              // value={singlebook['author']}
               className="form-control"
             />
             {errors.author?.type === "required" && "Author  is required" && (
--- pages/showBooks.tsx
+++ pages/showBooks.tsx
@@ -2,11 +2,11 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import { deleteBook, getAllBooks } from "../slices/bookSlice";
 
 const BooksView = () => {
-  const router = useRouter()
+  const router = useRouter();
   const AllBooks = useAppSelector(getAllBooks);
   // const count = useAppSelector(selectCount);
   // const numberOfBooks = useAppSelector((state) => state
@@ -19,7 +19,7 @@ const BooksView = () => {
       <h2 className="text-center">List of Books</h2>
       {/* <h2>There are {numberOfBooks} Books</h2> */}
       <table className="table table-striped table-border w-50 align-center m-auto">
-        <thead  className="text-center">
+        <thead className="text-center">
           <tr>
             {/* <th>ID</th> */}
             <th>Title</th>
@@ -28,7 +28,7 @@ const BooksView = () => {
           </tr>
         </thead>
         <tbody className="text-center">
-          {AllBooks.books.map((book) => {
+          {AllBooks["books"].map((book) => {
             const { id, title, author } = book;
             return (
               <tr key={id}>
@@ -36,10 +36,11 @@ const BooksView = () => {
                 <td>{title}</td>
                 <td>{author}</td>
                 <td>
-                  <Link href={`/editBook/${id}`} >
-                  <button className="btn btn-primary mx-2" >Edit</button>
+                  <Link href={`/editBook/${id}`}>
+                    <button className="btn btn-primary mx-2">Edit</button>
                   </Link>
-                  <button className="btn btn-info"
+                  <button
+                    className="btn btn-info"
                     onClick={() => {
                       handleDeleteBook(id);
                     }}
